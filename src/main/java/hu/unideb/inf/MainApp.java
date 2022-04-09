0f29b0cb8b790926e8c22e170c8fe27b00164b65
misi0 <galmihaly118@gmail.com>
2022-04-09 22:32:32 +0200

    mc1

---
+++ src/main/java/hu/unideb/inf/MainApp.java
@@ -0,0 +1,51 @@
+package hu.unideb.inf;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MainApp extends Application {
+
+    //mc1
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/login.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Bejelentkezés");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    //
+    // misi commit
+    public static void main(String[] args) {
+//
+
+        try {
+            startDatabase();
+        } catch (SQLException ex) {
+            Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+        launch(args);
+        stopDatabase();
+    }
+    //
+
+    private static final Server s = new Server();
+    
+    private static void startDatabase() throws SQLException {
+        s.runTool("-tcp", "-web", "-ifNotExists");
+    }
+
+    private static void stopDatabase()  {
+        s.shutdown();
+    }
+    
+}
---
+++ src/main/java/hu/unideb/inf/controller/RegisterController.java
@@ -0,0 +1,121 @@
+package hu.unideb.inf.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class RegisterController {
+
+    @FXML
+    private AnchorPane handleScreen;
+
+    @FXML
+    private TextField addressLabel;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField emailLabel;
+
+    @FXML
+    private TextField nameLabel;
+
+    @FXML
+    private TextField passwordAgainLabel;
+
+    @FXML
+    private TextField passwordLabel;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private TextField telefonnumberLabel;
+
+    @FXML
+    private Label noMatchingPass;
+
+    @FXML
+    private Label noValidAddress;
+
+    @FXML
+    private Label noValidEmail;
+
+    @FXML
+    private Label noValidName;
+
+    @FXML
+    private Label noValidPass;
+
+    @FXML
+    private Label noValidPhone;
+
+    @FXML
+    void clickedBackButton(ActionEvent event) {
+        Stage stage = (Stage) handleScreen.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void clickedRegistButton(ActionEvent event) {
+
+        boolean cont = true;
+        noValidName.setVisible(false);
+        noValidEmail.setVisible(false);
+        noValidAddress.setVisible(false);
+        noValidPhone.setVisible(false);
+        noValidPass.setVisible(false);
+        noMatchingPass.setVisible(false);
+        //eloszor nincs hibauzenet
+        if(nameLabel.getText().isEmpty()){
+            noValidName.setVisible(true);
+            cont = false;
+        }
+        if(addressLabel.getText().isEmpty()){
+            noValidAddress.setVisible(true);
+            cont = false;
+        }
+        if(telefonnumberLabel.getText().isEmpty()){
+            noValidPhone.setVisible(true);
+            cont = false;
+        }
+        if(!passwordLabel.getText().equals(passwordAgainLabel.getText())){
+            noMatchingPass.setVisible(true);
+            cont = false;
+        }
+        if(!emailLabel.getText().matches("^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")){
+            noValidEmail.setVisible(true);
+            cont = false;
+
+        }
+        if(!passwordLabel.getText().matches( "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$")){
+            noValidPass.setVisible(true);
+            Alert alert = new Alert(Alert.AlertType.ERROR,
+                    "A jelszónak tartalmaznia kell legalább egy kisbetűt, nagybetűt, számot, speciális karaktert, valamint 8 és 20 karakter közötti hosszúságúnak kell lennie!");
+            alert.showAndWait();
+            cont = false;
+        }
+
+
+        if(cont==true){
+            //
+            // do the register
+            //
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Sikeresen regisztrált!");
+            alert.showAndWait();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.close();
+
+        }
+    }
+
+    
+}
